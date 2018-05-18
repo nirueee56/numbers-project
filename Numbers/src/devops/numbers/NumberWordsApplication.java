54d28444c87a1c269599cb119d534affdbeadde9
niranjan.vennapusa <niru.holidays2017@gmail.com>
2018-05-18 14:41:05 +0000

    main class update

--- Numbers/src/devops/numbers/NumberWordsApplication.java
+++ Numbers/src/devops/numbers/NumberWordsApplication.java
@@ -19,11 +19,40 @@ public final class NumberWordsApplication {
 				System.out.print( "Enter number (0 to exit): " ) ;
 				String value = reader.readLine() ;
 				int number = Integer.parseInt( value ) ;
+				
+				if(number>=0 && number<=999){
+                    if(number==0){
+                        break;
+                    } else {
+                        System.out.print("NUMBER AFTER CONVERSION:\t");
+                        numberToWord(((number / 100) % 10), " HUNDRED");
+                        numberToWord((number % 100), " ");
+                    }
+
+                } else{
+                    System.out.print("NUMBER OUT OF RANGE");
+                }
+				
 			} catch ( NumberFormatException | IOException e ) {
 				System.out.println( "Invalid number" ) ;
 			}
 		}
 	}
+	
+	public static void numberToWord(int num, String val) {
+        String ones[] = {" ", " ONE", " TWO", " THREE", " FOUR", " FIVE", " SIX", " SEVEN", " EIGHT", " NINE", " TEN", " ELEVEN", " TWELVE", " THIRTEEN", " FOURTEEN", " FIFTEEN", " SIXTEEN", " SEVENTEEN", " EIGHTEEN", " NINETEEN"
+        };
+        String tens[] = {" ", " ", " TWENTY", " THIRTY", " FOURTY", " FIFTY", " SIXTY", " SEVENTY", " EIGHTY", " NINETY"};
+        if (num > 19) {
+            System.out.print(tens[num / 10] + " " + ones[num % 10]);
+        } else {
+            System.out.print(ones[num]);
+        }
+        if (num > 0) {
+            System.out.print(val);
+        }
+    }
+	
 
 	public static void main(String[] args) {
 		new NumberWordsApplication().execute() ;
